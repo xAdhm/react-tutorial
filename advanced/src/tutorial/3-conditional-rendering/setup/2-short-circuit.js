1f29ec7c8df850834cf88e1beedb01757460ac3b
Adham Mustafa <adhoomy.mustafa@gmail.com>
2025-11-09 19:05:44 -0500

    Update tutorial files and clean up

--- advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,16 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('');
+  const firstValue = text || 'hello world';
+  const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h1>{text || 'john doe'}</h1>
+      {text && <h1>hello world</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
